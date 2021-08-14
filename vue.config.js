12a6fc5dc043851d24c18bbd79273844c7e1429c
ashermorgan <59518073+ashermorgan@users.noreply.github.com>
2021-08-14 15:12:03 -0700

    Add icons and vue PWA plugin

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  publicPath: '',
+  pwa: {
+    name: 'running-calculator',
+    themeColor: '#ff8000',
+    msTileColor: '#ff8000',
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon-180x180.png',
+      msTileImage: 'img/icons/msapplication-icon-144x144.png'
+    },
+    manifestOptions: {
+      background_color: '#ff8000',
+      display: 'fullscreen',
+      lang: 'en-US'
+    }
+  }
+}
