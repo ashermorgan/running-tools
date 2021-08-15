49c5c1a5e59b4698abeb39ddf1fd8a4a83b5fded
ashermorgan <59518073+ashermorgan@users.noreply.github.com>
2021-08-15 09:23:41 -0700

    Add iOS splash screens

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,7 @@ module.exports = {
     name: 'running-calculator',
     themeColor: '#ff8000',
     msTileColor: '#ff8000',
+    appleMobileWebAppCapable: 'yes',
     iconPaths: {
       favicon32: 'img/icons/favicon-32x32.png',
       favicon16: 'img/icons/favicon-16x16.png',
