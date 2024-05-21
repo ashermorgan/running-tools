92e8f47c0df25202ff5b103a547935bbfd00f4b7
Asher Morgan <59518073+ashermorgan@users.noreply.github.com>
2024-05-20 20:29:30 -0700

    Update 404 page

--- src/views/NotFoundPage.vue
+++ src/views/NotFoundPage.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="not-found-page">
     <h1>404 Not Found</h1>
-    <p><router-link to="/home">homepage</router-link></p>
+    <p><router-link to="/home">Return home</router-link></p>
   </div>
 </template>
 
 <style scoped>
-h1 {
-  font-size: 1.5em;
-}
 .not-found-page {
   text-align: center;
+}
+.not-found-page h1 {
+  font-size: 1.5em;
+}
+.not-found-page p {
+  margin-top: 0.5em;
 }
 </style>
