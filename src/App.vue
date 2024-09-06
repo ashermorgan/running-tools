b7ee3e69737c9d9824182ae7fc3a5ce2fd577b8b
Asher Morgan <59518073+ashermorgan@users.noreply.github.com>
2024-09-06 09:35:17 -0700

    Add dark mode styles for 404.html

--- src/App.vue
+++ src/App.vue
@@ -27,38 +27,18 @@ import VueFeather from 'vue-feather';
   </div>
 </template>
 
-<style>
-header {
-  background-color: var(--theme);
-  padding: 0.5em;
-  display: grid;
-  grid-template-columns: 2em 1fr auto 1fr 2em;
-  grid-template-rows: auto;
-}
+<style scoped>
 header a {
   grid-column: 1;
   margin: auto;
   height: 2em;
   width: 2em;
 }
-::v-deep(.feather-chevron-left) {
+header ::v-deep(.feather-chevron-left) {
   padding: 0em;
-  color: #000000;
-}
-header h1 {
-  grid-column: 3;
-  font-size: 2em;
-  font-weight: bold;
-  text-decoration: none;
   color: #000000;
 }
 #route-content {
   margin: 1em;
-}
-@media only screen and (max-width: 450px) {
-  /* adjust title size to fit small devices */
-  header h1 {
-    font-size: 7vw;
-  }
 }
 </style>
